f4d53ee76701929c7a83fed8a1509c6c2c212321
windyashes <sethkemmet@gmail.com>
2024-08-18 22:00:24 -0500

    Added showCollection & findByArtist functions

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,19 +12,37 @@ function addToCollection(collection, title, artist, yearPublished){
  collection.push(album);
  return album;
 }
-
+function showCollection(collection){
+  for(let i = 0; i < collection.length; i++){
+    console.log(`"${collection[i].title}" by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
+  }
+}
+function findByArtist(collection, artist){
+  let matches = [];
+  console.log(`Looking for albums by ${artist}...`);
+  for(let i = 0; i < collection.length; i++){
+    if(collection[i].artist === artist){
+      matches.push(collection[i]);
+    }
+  }
+  if(matches == []){
+    console.log('None found.');
+  }
+  return matches;
+}
 console.log(myCollection);
-addToCollection('Census Designated', 'Jane Remover', '2023');
-addToCollection('Flash in the Pan', 'Jane Remover', '2024');
-addToCollection('Remote Echoes', 'Duster', '2023');
-addToCollection('Moods, Modes', 'Duster', '2023');
-addToCollection('AMAMA', 'Crumb', '2024');
-addToCollection('King of Hearts', 'Camu Tao', '2010');
+addToCollection(myCollection, 'Census Designated', 'Jane Remover', '2023');
+addToCollection(myCollection, 'Flash in the Pan', 'Jane Remover', '2024');
+addToCollection(myCollection, 'Remote Echoes', 'Duster', '2023');
+addToCollection(myCollection, 'Moods, Modes', 'Duster', '2023');
+addToCollection(myCollection, 'AMAMA', 'Crumb', '2024');
+addToCollection(myCollection, 'King of Hearts', 'Camu Tao', '2010');
 console.log(myCollection);
+showCollection(myCollection);
 
-
-
-
+showCollection(findByArtist(myCollection, 'Crumb'));
+showCollection(findByArtist(myCollection, 'Jane Remover'));
+showCollection(findByArtist(myCollection, 'The Breathing Effect'));
 
 
 
